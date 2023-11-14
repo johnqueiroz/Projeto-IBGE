aeea94f810ae0dd75a8ed73550f030cce37ce318
johnqueiroz <johnemerson67@gmail.com>
2023-11-14 18:21:05 -0300

    Tela feita para listar os equipamentos e incremento da função de listagem dos equipamentos

--- equipamentos/funcEquipamentos.php
+++ equipamentos/funcEquipamentos.php
@@ -9,7 +9,7 @@ class funcEquipamentos extends Conexao{
 
     private string $IdServidor;
     
-    public function coletarDadosEquipamentos(){
+    public function coletarQuantEquipamentos(){
 
         $this->conn = $this->conectarBD();
 
@@ -38,6 +38,50 @@ class funcEquipamentos extends Conexao{
             echo $erro->getMessage();
         }finally{
             return $prepararQuery->fetch(PDO::FETCH_ASSOC);
+        }
+    }
+
+
+
+    public function coletarDadosEquipamentos(){
+
+        $this->conn = $this->conectarBD();
+
+
+        $userEmail = $_SESSION['userEmail'];
+        $IdServidor = $_SESSION['IdServidor'];
+
+
+        $query = "SELECT 
+        eqp.patrimonio,
+        tipoeqp.tipo AS tipoEquipamento,
+        area.nomeArea,
+        statuseqp.status,
+        user.nomeServidor,
+        DATE_FORMAT(eqp.dataMovimentacao, '%d/%m/%Y') as dataMovimentacao
+        FROM 
+        equipamentos AS eqp 
+        INNER JOIN
+        tipoequipamento AS tipoeqp ON eqp.IdTipo = tipoeqp.idTipoEquipamento 
+        INNER JOIN 
+        area ON eqp.IdArea = area.Id
+        INNER JOIN
+        statusequipamento as statuseqp ON eqp.IdStatus = statuseqp.idStatusEquipamento 
+        INNER JOIN 
+        user ON eqp.IdServidor = user.IdServidor 
+        WHERE eqp.IdServidor = :IdServidor";
+        
+        
+        $prepararQuery = $this->conn->prepare($query);
+       
+        $prepararQuery->bindParam(':IdServidor', $IdServidor, PDO::PARAM_INT);
+        
+        try{
+            $prepararQuery->execute();
+        }catch (PDOException $erro){
+            echo $erro->getMessage();
+        }finally{
+            return $prepararQuery->fetchAll(PDO::FETCH_ASSOC);
         }
     }
 
