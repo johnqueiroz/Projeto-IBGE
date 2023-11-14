c92c8ce325a43c5d3fc063912e3054d5024959ba
johnqueiroz <johnemerson67@gmail.com>
2023-11-14 18:20:10 -0300

    Separei para ficar mais fácil de reutilizar

---
+++ telaPadrao.php
@@ -0,0 +1,86 @@
+<?php
+// Essa página só deve aparecer caso o usuário esteja logado
+// Dessa forma inicia a sessão para verficação
+session_start();
+
+// Verificar se o usuário está logado ou não
+if (!isset($_SESSION['userEmail'])) {
+// Redirecionar para a página de login caso não esteja
+    header("Location: ../login/entrada.php");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="../css/estiloNavBar.css" type="text/css">
+    <title>Lista de equipamentos</title>
+    
+</head>
+
+<?php
+// Faz uso da classe de usuário para coletar os dados de usuário para utilizar o nome na página
+include_once '../Users/Cadastros/user.php';
+
+$createUser = new user();
+$dadosUsuario = $createUser->coletarDadosUser();
+
+$_SESSION['IdServidor'] = $dadosUsuario['IdServidor'];
+
+?>
+
+<body>
+    <!-- Inicio Navbar -->
+    <nav class="navbar">
+        <div class="navbar-content">
+            <div class="bars">
+                <i class="fa-solid fa-bars fa-xl"></i>
+            </div>
+            <b><span class="logo">IBGE</span></b>
+        </div>
+
+        <div class="navbar-content">
+            <div class="notification">
+                
+            </div>
+
+            <div class="avatar">
+                <img src="../images/profilePic.jpeg" alt="">
+                <b><?php echo $dadosUsuario['nomeServidor'];?></b>
+                <div class="dropdown-menu setting">
+                    <div class="item">
+                        <span class="fa-solid fa-user"></span> Perfil
+                    </div>
+                    <div class="item" onclick="encerrar()">
+                        <span class="fa-solid fa-arrow-right-from-bracket"></span> Sair
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <!-- Fim Navbar -->
+
+    <!-- Inicio Conteudo -->
+    <div class="content">
+        <!-- Inicio da Sidebar -->
+        <div class="sidebar">
+            <a href="dashboard.php" class="sidebar-nav active"><i class="icon fa-solid fa-house"></i><span>Dashboard</span></a>
+
+            <a href="#" class="sidebar-nav"><i class="icon fa-solid fa-users"></i><span>Listar</span></a>
+
+            <a href="#" class="sidebar-nav"><i class="icon fa-solid fa-arrow-right-from-bracket"></i><span>Sair</span></a>
+
+        </div>
+
+
+    <script src="../js/index.js"></script>  
+    <script src="../js/inatividade.js"></script>      
+</body>
+
+
+</html>        
